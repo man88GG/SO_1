f25445886d66cb76267e0bd01fac50618cb9aec8
Mr_jst21 <stmario.campos@gmail.com>
2019-10-01 07:35:01 -0600

    Diseño_Base

---
+++ Simulacion_Procesos/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Simulacion_Procesos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.IconCerrar = new System.Windows.Forms.PictureBox();
+            this.IconMin = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TxtActualizar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconMin)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.IconMin);
+            this.panel1.Controls.Add(this.IconCerrar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 40);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            // 
+            // IconCerrar
+            // 
+            this.IconCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.IconCerrar.BackColor = System.Drawing.Color.White;
+            this.IconCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.IconCerrar.Image = ((System.Drawing.Image)(resources.GetObject("IconCerrar.Image")));
+            this.IconCerrar.Location = new System.Drawing.Point(961, 3);
+            this.IconCerrar.Name = "IconCerrar";
+            this.IconCerrar.Size = new System.Drawing.Size(36, 32);
+            this.IconCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.IconCerrar.TabIndex = 1;
+            this.IconCerrar.TabStop = false;
+            this.IconCerrar.Click += new System.EventHandler(this.IconCerrar_Click);
+            // 
+            // IconMin
+            // 
+            this.IconMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.IconMin.BackColor = System.Drawing.Color.White;
+            this.IconMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.IconMin.Image = ((System.Drawing.Image)(resources.GetObject("IconMin.Image")));
+            this.IconMin.Location = new System.Drawing.Point(919, 3);
+            this.IconMin.Name = "IconMin";
+            this.IconMin.Size = new System.Drawing.Size(36, 32);
+            this.IconMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.IconMin.TabIndex = 2;
+            this.IconMin.TabStop = false;
+            this.IconMin.Click += new System.EventHandler(this.IconMin_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.TxtActualizar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(222, 460);
+            this.panel2.TabIndex = 1;
+            // 
+            // TxtActualizar
+            // 
+            this.TxtActualizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TxtActualizar.FlatAppearance.BorderSize = 0;
+            this.TxtActualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.TxtActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TxtActualizar.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtActualizar.ForeColor = System.Drawing.Color.Black;
+            this.TxtActualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.TxtActualizar.Location = new System.Drawing.Point(0, 0);
+            this.TxtActualizar.Name = "TxtActualizar";
+            this.TxtActualizar.Size = new System.Drawing.Size(222, 60);
+            this.TxtActualizar.TabIndex = 0;
+            this.TxtActualizar.Text = "ACTUALIZAR";
+            this.TxtActualizar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OTRA COSA XD";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(0, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "BORRAR PROCESO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ADMINISTRADOR DE PROCESOS GRUPO #4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconMin)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox IconMin;
+        private System.Windows.Forms.PictureBox IconCerrar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button TxtActualizar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
